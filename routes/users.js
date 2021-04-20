8ac1372daba4e9da3c50d3462a176cc4be6ecb3e
Durre shewar Malik <durreshewarmalik@Durres-MacBook-Pro.local>
2021-04-20 19:24:43 +0200

    getting the data

---
+++ routes/users.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+const fs = require("fs");
+const cors = require("cors");
+
+router.use(cors());
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+
+  fs.readFile("users.json", (err, data) => {
+    if (err){
+      console.log(err);
+    };
+
+    let users = JSON.parse(data);
+
+    res.json(users);
+  });
+});
+
+module.exports = router;
