baaac0582a6546ab2efa0af1dae8dddf223b6257
Durre shewar Malik <durreshewarmalik@Durres-MacBook-Pro.local>
2021-04-22 23:52:03 +0200

    some cleaning

--- routes/users.js
+++ routes/users.js
@@ -32,8 +32,6 @@ router.post("/new", (req, res) => {
 
  console.log(result);
 
- 
-
  res.redirect("/");
 
   });
@@ -45,36 +43,16 @@ router.post("/login", (req, res) => {
   let user = req.body;
   
   let password = cryptoJS.AES.decrypt(user.password, "bismillah").toString();
+  
   req.app.locals.db.collection("users").find({"username": user.username, "password": password, subscription: false}).toArray()
 
   .then(result=>{
 
  console.log(result);
 
- 
-
  res.json(result && result.length && result[0]);
 
   });
-
-    // let users = JSON.parse(data);
-
-    // let findUser = users.find((users) => users.username == user.username && users.password == user.password);
-    
-    // if (findUser){
-    //   let data = {username: findUser.username, subscription: findUser.subscription, id: findUser.id};
-    //   res.json(data)
-    // } else {
-    //   res.send("Wrong details")
-    // }
-
-    // Databas kopplat, då köra koden nedan
-    // users.find({"username": user.username},{"password": user.password}).toArray()
-    // .then(results => {
-    //      console.log(results)
-    // });
-
-   });
-// });
+});
 
 module.exports = router;
